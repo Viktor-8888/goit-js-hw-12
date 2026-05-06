733f2e6f698b6c8f64c8a35c085c5c4afa48f7a5
Viktor_Shcherbyna <ahmatov@ukr.net>
2026-05-06 15:57:50 +0300

    edited Homework-12

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,14 +1,16 @@
 import axios from 'axios';
 
-const key = '49187044-c2c4cc5e7c1f3c23966a70411';
-export function search(query) {
-  return axios.get('https://pixabay.com/api/', {
+export async function getImagesByQuery(query, page) {
+  const response = await axios.get('https://pixabay.com/api/', {
     params: {
-      key,
+      key: '49187044-c2c4cc5e7c1f3c23966a70411',
+      page: page,
+      per_page: 15,
       q: query,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
     },
   });
+  return response.data;
 }
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,68 +1,69 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
 const list = document.querySelector('.gallery');
-
+export const buttonLoadMore = document.querySelector('.hidden');
 let gallery = new SimpleLightbox('.gallery a', {
   captions: true,
   captionPosition: 'bottom',
   captionDelay: 250,
   captionsData: 'alt',
 });
-
-export function renderImg(hits) {
-  const items = hits.map(hit => {
-    const li = document.createElement('li');
-    li.classList.add('gallery-item');
-    const link = document.createElement('a');
-    link.classList.add('gallery-link');
-    link.href = hit.largeImageURL;
-    li.append(link);
-    const img = document.createElement('img');
-    img.classList.add('galleryi-item-img');
-    img.src = hit.webformatURL;
-    img.alt = hit.tags;
-    link.append(img);
-
-    const imgInfo = document.createElement('ul');
-    imgInfo.classList.add('gallery-link-list');
-    link.append(imgInfo);
-
-    const infoLikes = document.createElement('li');
-    infoLikes.textContent = 'Likes';
-    const infoLikesSpan = document.createElement('span');
-    infoLikesSpan.textContent = hit.likes;
-    infoLikes.append(infoLikesSpan);
-    imgInfo.append(infoLikes);
-
-    const infoViews = document.createElement('li');
-    infoViews.textContent = 'Views';
-    const infoViewsSpan = document.createElement('span');
-    infoViewsSpan.textContent = hit.views;
-    infoViews.append(infoViewsSpan);
-    imgInfo.append(infoViews);
-
-    const infoComments = document.createElement('li');
-    infoComments.textContent = 'Comments';
-    const infoCommentsSpan = document.createElement('span');
-    infoCommentsSpan.textContent = hit.comments;
-    infoComments.append(infoCommentsSpan);
-    imgInfo.append(infoComments);
-
-    const infoDownloads = document.createElement('li');
-    infoDownloads.textContent = 'Downloads';
-    const infoDownloadsSpan = document.createElement('span');
-    infoDownloadsSpan.textContent = hit.downloads;
-    infoDownloads.append(infoDownloadsSpan);
-    imgInfo.append(infoDownloads);
-
-    return li;
-  });
-  list.append(...items);
-
+export function createGallery(images) {
+  images.forEach(
+    ({
+      webformatURL,
+      largeImageURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+    }) =>
+      list.insertAdjacentHTML(
+        'beforeend',
+        `<li class="gallery-item">
+  <a href="${largeImageURL}" class="gallery-link">
+    <img src="${webformatURL}" alt="${tags}" class="gallery-image" />
+    <ul class="gallery-text">
+      <li class="gallery-text-item">
+        Likes<span class="gallery-span-item">${likes}</span>
+      </li>
+      <li class="gallery-text-item">
+        Views<span class="gallery-span-item">${views}</span>
+      </li>
+      <li class="gallery-text-item">
+        Comments<span class="gallery-span-item">${comments}</span>
+      </li>
+      <li class="gallery-text-item">Downloads<span class="gallery-span-item">${downloads}</span></li>
+    </ul>
+  </a>
+</li>`
+      )
+  );
   gallery.refresh();
 }
-
-export function clear() {
-  list.innerHTML = '';
+export function clearGallery() {
+  list.innerHTML = ``;
+}
+export function showLoader() {
+  buttonLoadMore.insertAdjacentHTML('afterend', `<span class="loader"></span>`);
+}
+export function hideLoader() {
+  const span = document.querySelector('.loader');
+  if (span) span.remove();
+}
+export function showLoadMoreButton() {
+  buttonLoadMore.classList.add('gallery-button');
+}
+export function hideLoadMoreButton() {
+  buttonLoadMore.classList.remove('gallery-button');
+}
+export function scrolling() {
+  const li = list.querySelector('.gallery-item');
+  const { height } = li.getBoundingClientRect();
+  window.scrollBy({
+    top: height * 2,
+    left: 0,
+    behavior: 'smooth',
+  });
 }
